ac791f4046946e703c473dae1b14c6020c909a0e
matthewmirror1 <matuespejo@gmail.com>
2023-06-02 13:57:42 -0300

    Clase Centro

---
+++ TP FINAL/cCentro.cpp
@@ -0,0 +1,13 @@
+#include "cCentro.h"
+
+cCentro::cCentro(string nombre, string direccion, string telefono)
+{
+	this->Direccion = direccion;
+	this->Nombre = nombre;
+	this->Telefono=telefono;
+
+}
+
+cCentro::~cCentro()
+{
+}
---
+++ TP FINAL/cCentro.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Header.h"
+class cCentro {
+
+public:
+	cCentro(string nombre, string direccion, string telefono);
+	~cCentro();
+	
+
+private:
+	string Nombre;
+	string Direccion;
+	string Telefono;
+};
